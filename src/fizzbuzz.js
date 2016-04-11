42155466e2f1eb0d7437fb21f4ec613b0f3454ea
Johan Martinsson <j.martinsson@bee-buzziness.com>
2016-04-12 00:21:16 +0200

    imperative solution with two ifs

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -10,21 +10,23 @@ FizzBuzz.prototype.say = function(number)
     function isDividableBy(i) {
         return function(number) {return number % i == 0}
     }
-
     var specialExpressionMatchers = [
         {matches: isDividableBy(3), word: "fizz"},
         {matches: isDividableBy(5), word: "buzz"},
         {matches: isDividableBy(7), word: "bang"},
     ]
 
-    var matchingExpressions = specialExpressionMatchers.filter(matcher => matcher.matches(number))
+    var result = ''
+    specialExpressionMatchers.forEach(matcher => {
+        if (matcher.matches(number)) {
+            result += matcher.word
+        }
+    })
 
-    var concatenatedSpecialCases = matchingExpressions.map(expression => expression.word).join('')
+    if (result) {
+        return result
+    } else {
+        return number.toString()
+    }
 
-    // this would probably be clearer
-    // return concatenatedSpecialCases ? concatenatedSpecialCases : number.toString()
-    //
-    // but the functional style would be:
-    var firstNonEmptyStringOf = _.find;
-    return firstNonEmptyStringOf([concatenatedSpecialCases, number.toString()])
 }
