eadeff2a07d396c4e541e161caf305aa22af17fb
Johan Martinsson <j.martinsson@bee-buzziness.com>
2016-04-12 00:35:15 +0200

    introduce fizzbuzzComposer

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,23 +1,13 @@
-function FizzBuzz()
+function FizzBuzz(specialExpressionMatchers)
 {
-    
+    this.specialExpressionMatchers = specialExpressionMatchers
 }
 
 
 FizzBuzz.prototype.say = function(number)
 {
 
-    function isDividableBy(i) {
-        return function(number) {return number % i == 0}
-    }
-
-    var specialExpressionMatchers = [
-        {matches: isDividableBy(3), word: "fizz"},
-        {matches: isDividableBy(5), word: "buzz"},
-        {matches: isDividableBy(7), word: "bang"},
-    ]
-
-    var matchingExpressions = specialExpressionMatchers.filter(matcher => matcher.matches(number))
+    var matchingExpressions = this.specialExpressionMatchers.filter(matcher => matcher.matches(number))
 
     var concatenatedSpecialCases = matchingExpressions.map(expression => expression.word).join('')
 
